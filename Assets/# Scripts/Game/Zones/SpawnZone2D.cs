96bef0a529f2381a3e03cd93c27563c6beb43462
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-26 18:41:19 +0300

    Create SpawnZone2D.cs

---
+++ Assets/# Scripts/Game/Zones/SpawnZone2D.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// Скрипт спавнит переданный ему объект в зоне 2д коллайдера
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class SpawnZone2D : MonoBehaviour
+{
+    // Кол-во попыток спавна объекта в рамках 2д зоны (круг, элипс, квадрад, прямоугольних и т.п.)
+    private int maxAttempts = 100;
+
+    private Collider2D _spawnArea;
+
+
+    private void Awake()
+    {
+        _spawnArea = GetComponent<Collider2D>();
+    }
+
+
+    public void Spawn(GameObject prefabToSpawn)
+    {
+        Bounds bounds = _spawnArea.bounds;
+
+        for (int i = 0; i < maxAttempts; i++)
+        {
+            Vector2 randomPoint = new Vector2(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y)
+            );
+
+            if (_spawnArea.OverlapPoint(randomPoint))
+            {
+                Instantiate(prefabToSpawn, randomPoint, Quaternion.identity);
+                return;
+            }
+        }
+
+        Debug.LogWarning("Не удалось найти подходящую точку для спавна");
+    }
+}
