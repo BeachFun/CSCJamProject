c45b3cc10eb05e3133aea89fc71a280b91784c8a
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-26 15:44:50 +0300

    Update GameManager | add gamestate logic

--- Assets/# Scripts/Game/Managers/GameManager.cs
+++ Assets/# Scripts/Game/Managers/GameManager.cs
@@ -1,15 +1,38 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private GameState _gameState;
+
+
+    public event Action OnPlayed;
+    public event Action OnPaused;
+
+
     private void Start()
     {
+        ChangeGameState(_gameState);
+
         Debug.Log("Game Manager is Started");
     }
 
+
     public void ChangeGameState(GameState state)
     {
+        _gameState = state;
 
+        switch (state)
+        {
+            case GameState.Paused:
+                OnPlayed?.Invoke();
+                break;
+            case GameState.Played:
+                OnPlayed?.Invoke();
+                break;
+
+            default: break;
+        }
     }
 
     public void ExitGame()
--- Assets/# Scripts/Presentation/UI/PauseMenuControllerUI.cs
+++ Assets/# Scripts/Presentation/UI/PauseMenuControllerUI.cs
@@ -6,6 +6,24 @@ public class PauseMenuControllerUI : MonoBehaviour
     [Inject] GameManager gameManager;
     [Inject] UIService uiService;
 
+    private void Awake()
+    {
+        if (gameManager is null) return;
+
+        gameManager.OnPlayed += Hide;
+        gameManager.OnPaused += Show;
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
     public void Resume()
     {
         gameManager?.ChangeGameState(GameState.Played);
