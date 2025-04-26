5bc1f0a879e6b563cf501fb15b29a6d3e16755f4
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-27 02:19:58 +0300

    Create Base Zombie and damage system

---
+++ Assets/# Scripts/Game/Zones/EnemyTriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyTriggerZone : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnTriggerEnter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<ZombieController>().Kill();
+            OnTriggerEnter?.Invoke();
+        }
+    }
+}
