a4a3e9bc8f54780f6f74dec065ff74d3c0b7060f
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-26 15:26:21 +0300

    Create GameManager

---
+++ Assets/# Scripts/Game/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Game Manager is Started");
+    }
+
+    public void ChangeGameState(GameState state)
+    {
+
+    }
+
+    public void ExitGame()
+    {
+
+    }
+}
---
+++ Assets/# Scripts/Zenject Installers/GameManagersInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Zenject;
+
+public class GameManagersInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<GameManager>().FromComponentInHierarchy().AsSingle().NonLazy();
+    }
+}
