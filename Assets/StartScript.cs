39ba83d029c101195c8d6d80e90320dc2be28a11
Kenandchikusa <shaita4@gmail.com>
2025-04-27 13:09:36 +0300

    1

--- Assets/StartScript.cs
+++ Assets/StartScript.cs
@@ -3,8 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
-    public void OnPlayButton()
+    public void OnPlayButton1()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void OnPlayButton2()
+    {
+        SceneManager.LoadScene("Level 2");
     }
 }
