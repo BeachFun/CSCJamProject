b84e5a2927fda08e6089a73eae229671d1486f77
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-27 09:40:07 +0300

    Some changes 1

--- Assets/# Scripts/Presentation/UI/Animathion.cs
+++ Assets/# Scripts/Presentation/UI/Animathion.cs
@@ -7,7 +7,7 @@ public class NewMonoBehaviourScript : MonoBehaviour, IPointerEnterHandler, IPoin
 {
     [Header("Settings")]
     [SerializeField] private Vector3 _hoverScale = new Vector3(1.1f, 1.1f, 1.0f);
-    [SerializeField] private float _animationTime = .4f;
+    [SerializeField] private float _animationDuration = .2f;
 
     private Vector3 _originalScale;
 
@@ -18,11 +18,11 @@ public class NewMonoBehaviourScript : MonoBehaviour, IPointerEnterHandler, IPoin
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.DOScale(_hoverScale, _animationTime);
+        transform.DOScale(_hoverScale, _animationDuration);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.DOScale(_originalScale, _animationTime / 2);
+        transform.DOScale(_originalScale, _animationDuration / 2);
     }
 }
