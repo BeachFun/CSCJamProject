bfd351be7b2021d35894305a98c239270304668b
PROGRAMISTO <habib2004habib@yandex.ru>
2025-05-19 10:44:45 +0300

    Update ToggleButton and ToggleGroup

---
+++ Assets/# Scripts/Game/ToggleButton2D.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using Zenject;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class ToggleButton2D : MonoBehaviour
+{
+    [Header("Settgins")]
+    [SerializeField] private InstrumentEnum _instrument;
+    [SerializeField] private int _road;
+
+    [Header("References")]
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite highlightedSprite;
+    [SerializeField] private Sprite pressedSprite;
+
+    [Header("Group")]
+    [SerializeField] private ToggleButton2DGroup group;
+
+    [Inject] private InstrumentManager instrumentManager;
+
+    private SpriteRenderer spriteRenderer;
+
+    public bool IsPressed { get; private set; }
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = normalSprite;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!IsPressed)
+            spriteRenderer.sprite = highlightedSprite;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!IsPressed)
+            spriteRenderer.sprite = normalSprite;
+    }
+
+    private void OnMouseDown()
+    {
+        group.OnButtonClicked(this);
+    }
+
+    public void SetPressed(bool pressed)
+    {
+        IsPressed = pressed;
+
+        if (IsPressed)
+            spriteRenderer.sprite = pressedSprite;
+        else
+            spriteRenderer.sprite = normalSprite;
+
+        instrumentManager.ChangeRoad(_instrument, _road);
+    }
+}
---
+++ Assets/# Scripts/Game/ToggleButton2DGroup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class ToggleButton2DGroup : MonoBehaviour
+{
+    [Header("Settgins")]
+    [SerializeField] private InstrumentEnum _instrument;
+
+    [Inject] private InstrumentManager instrumentManager;
+
+    private ToggleButton2D _currentToggled;
+
+
+    public void OnButtonClicked(ToggleButton2D clickedButton)
+    {
+        // Деактивация
+        if (_currentToggled == clickedButton)
+        {
+            clickedButton.SetPressed(false);
+            _currentToggled = null;
+
+            instrumentManager.SetInstrumentActive(_instrument, false);
+        }
+        // Активация
+        else
+        {
+            if (_currentToggled != null)
+                _currentToggled.SetPressed(false);
+
+            clickedButton.SetPressed(true);
+            _currentToggled = clickedButton;
+
+            instrumentManager.SetInstrumentActive(_instrument, true);
+        }
+    }
+}
