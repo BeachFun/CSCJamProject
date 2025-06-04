79e2df5acb7d0c7721004e796cfb0f63dcab6318
PROGRAMISTO <habib.programmer@yandex.ru>
2025-06-04 21:49:58 +0300

    Update Loading Screen

--- Assets/# Scripts/Presentation/Screens/LoadingScreen.cs
+++ Assets/# Scripts/Presentation/Screens/LoadingScreen.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class LoadingScreen : ScreenBase, IDataUpdatable<float>
 {
+    [Header("Settings")]
+    [SerializeField] private string ConfirmLabel;
     [Header("Bindings")]
     [SerializeField] private ProgressBar progressBar;
     [SerializeField] private TMP_Text textNeedConfirm;
 
+
+    protected override void Awake()
+    {
+        base.Awake();
+        textNeedConfirm?.SetText(ConfirmLabel);
+    }
+
+    private void Start()
+    {
+        progressBar.Refresh();
+    }
 
     private void OnEnable() => ShowConfirmLabel(false);
     private void OnDisable() => ShowConfirmLabel(false);
