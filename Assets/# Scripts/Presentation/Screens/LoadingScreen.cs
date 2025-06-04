6c4384679e2e3f8ca66651dd417554b7d211b101
PROGRAMISTO <habib2004habib@yandex.ru>
2025-06-04 21:01:33 +0300

    Create Loading Screen

---
+++ Assets/# Scripts/Presentation/Screens/LoadingScreen.cs
@@ -0,0 +1,29 @@
+﻿using FriendNote.UI;
+using TMPro;
+using UnityEngine;
+
+public class LoadingScreen : ScreenBase, IDataUpdatable<float>
+{
+    [Header("Bindings")]
+    [SerializeField] private ProgressBar progressBar;
+    [SerializeField] private TMP_Text textNeedConfirm;
+
+
+    private void OnEnable() => ShowConfirmLabel(false);
+    private void OnDisable() => ShowConfirmLabel(false);
+
+
+    public void UpdateData(float data)
+    {
+        progressBar.UpdateData(data);
+    }
+
+    /// <summary>
+    /// Переключение бара загрузки на надпись о подтверждении
+    /// </summary>
+    public void ShowConfirmLabel(bool isShow)
+    {
+        progressBar.gameObject.SetActive(!isShow);
+        textNeedConfirm.gameObject.SetActive(isShow);
+    }
+}
