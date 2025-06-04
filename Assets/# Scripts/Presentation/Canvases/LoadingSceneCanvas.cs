31bb6e21a01231fa96e8583004e1bc3c9bf517a8
PROGRAMISTO <habib.programmer@yandex.ru>
2025-06-04 21:52:18 +0300

    Create LoadingScene Canvas

---
+++ Assets/# Scripts/Presentation/Canvases/LoadingSceneCanvas.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UniRx;
+using Zenject;
+using System.Collections;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// Менеджер управления отображением загрузки на сцене Loading
+/// </summary>
+public class LoadingSceneCanvas : CanvasBase
+{
+    [Header("Bindings")]
+    [SerializeField] private LoadingScreen screen;
+
+    [Inject] private SceneLoadingService loadingService;
+
+    private void Awake()
+    {
+        loadingService.IsLoading
+            .Subscribe(screen.Show)
+            .AddTo(this);
+
+        loadingService.Progress
+            .Subscribe(screen.UpdateData)
+            .AddTo(this);
+
+        loadingService.IsLoadedForConfirm
+            .Where(isShow => isShow == true)
+            .Subscribe(x => ConfirmShow())
+            .AddTo(this);
+    }
+
+    private void ConfirmShow()
+    {
+        screen.ShowConfirmLabel(true);
+        WaitForAnyKey(loadingService).Forget();
+    }
+
+    public static async UniTaskVoid WaitForAnyKey(SceneLoadingService service)
+    {
+        await UniTask.WaitUntil(() => Input.anyKeyDown);
+        service.ConfirmActivation();
+    }
+}
