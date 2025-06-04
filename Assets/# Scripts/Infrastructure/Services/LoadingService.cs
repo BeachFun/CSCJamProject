acdfa41245bbdb7f6eb1375699a238c5278d4610
PROGRAMISTO <habib.programmer@yandex.ru>
2025-06-04 21:57:19 +0300

    Create Simple Loading Ssytem

---
+++ Assets/# Scripts/Infrastructure/Services/LoadingService.cs
@@ -0,0 +1,31 @@
+﻿using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingService
+{
+    // TODO: доработать
+
+    private ReactiveProperty<bool> isLoading = new();
+    private ReactiveProperty<float> progress = new();
+    private ReactiveProperty<bool> isLoaded = new();
+
+    public ReadOnlyReactiveProperty<bool> IsLoading => isLoading.ToReadOnlyReactiveProperty();
+    public ReadOnlyReactiveProperty<float> Progress => progress.ToReadOnlyReactiveProperty();
+    public ReadOnlyReactiveProperty<bool> IsLoaded => isLoaded.ToReadOnlyReactiveProperty();
+
+
+    public void LoadScene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single)
+    {
+        SceneManager.LoadScene("Loading", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(sceneName, mode);
+
+        //UniTask.Run();
+    }
+
+    private void ReadProgress(AsyncOperation operation)
+    {
+
+    }
+}
---
+++ Assets/# Scripts/Presentation/Canvases/LoadingCanvas.cs
@@ -0,0 +1,32 @@
+﻿using Zenject;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// Менеджер управления загрузкой
+/// </summary>
+public class LoadingCanvas : CanvasBase
+{
+    // TODO: доработать
+
+    [Header("Settings")]
+    [SerializeField] private bool dontDestroyOnLoad = true;
+    [Header("Bindings")]
+    [SerializeField] private LoadingScreen screen;
+
+    [Inject] private LoadingService loadingService;
+
+    private void Awake()
+    {
+        if (dontDestroyOnLoad)
+            DontDestroyOnLoad(this.gameObject);
+
+        loadingService.IsLoading
+            .Subscribe(Show)
+            .AddTo(this);
+
+        loadingService.Progress
+            .Subscribe(screen.UpdateData)
+            .AddTo(this);
+    }
+}
