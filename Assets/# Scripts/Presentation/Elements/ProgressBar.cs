9d3372b57f236b158fd7fead27b3b51d933c8631
PROGRAMISTO <habib2004habib@yandex.ru>
2025-06-04 21:00:12 +0300

    Create ProgressBar

---
+++ Assets/# Scripts/Presentation/Elements/ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using FriendNote.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour, IDataUpdatable<float>, IRefreshable
+{
+    [Header("Settings")]
+    [Range(0f, .5f)]
+    [SerializeField] private float animationSmoothDuration = 0.1f;
+    [Header("Bindings")]
+    [SerializeField] private TMP_Text textBar;
+    [SerializeField] private Image imageProggres;
+
+    private float progress;
+
+    public void Refresh()
+    {
+        textBar?.SetText(progress.ToString("P0"));
+        imageProggres?.DOFillAmount(progress, animationSmoothDuration);
+    }
+
+    public void UpdateData(float data)
+    {
+        progress = data;
+        Refresh();
+    }
+}
